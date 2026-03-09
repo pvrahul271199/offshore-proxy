b8350656a5ff432dbc04b91e79bc0059f712cfdb
Rahul P V <rahulpv@Rahuls-MacBook.local>
2026-03-09 21:59:52 +0530

    project structure

---
+++ proxyclient/src/test/java/com/segments/proxyclient/ProxyclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segments.proxyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
